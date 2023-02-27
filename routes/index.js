32d1b2211bfd4cc60d902269877da0e0bafd77b1
SeberyDev <sebasmanager06@gmail.com>
2023-02-27 12:56:55 -0600

    Update homepage title and view

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Code Engine" });
 });
 
 router.get("/mysearch", function (req, res, next) {
-  res.render("mysearch", { title: "mysearch" });
+  res.render("userSearch", { title: "My Searchs" });
 });
 
 module.exports = router;
